9fb8e68f6995b778d39a1796d1063757c50cc3da
Estanislao Caputto <caputto.estanislao@hotmail.com>
2021-06-10 17:31:12 -0300

    ultimo avance

---
+++ js/botones.js
@@ -0,0 +1,39 @@
+let boton1 = document.getElementById("1")
+      boton1.onclick = () =>{agregarTabaco(1)};
+
+let boton2 = document.getElementById("2")
+      boton2.onclick = () =>{agregarTabaco(2)};
+
+let boton3 = document.getElementById("3")
+      boton3.onclick = () =>{agregarTabaco(3)};
+
+let boton4 = document.getElementById("4")
+      boton4.onclick = () =>{agregarTabaco(4)};
+
+let boton5 = document.getElementById("5")
+      boton5.onclick = () =>{agregarCedas(5)};
+
+      
+let boton6 = document.getElementById("6")
+      boton6.onclick = () =>{agregarCedas(6)};
+
+let boton7 = document.getElementById("7")
+      boton7.onclick = () =>{agregarCedas(7)};
+
+let boton8 = document.getElementById("8")
+      boton8.onclick = () =>{agregarCedas(8)};
+
+
+let boton9 = document.getElementById("9")
+      boton9.onclick = () =>{agregarFiltros(9)};
+
+
+let boton10 = document.getElementById("10")
+      boton10.onclick = () =>{agregarFiltros(10)};
+
+
+let boton11 = document.getElementById("11")
+      boton11.onclick = () =>{agregarFiltros(11)};
+
+let boton12 = document.getElementById("12");
+      boton12.onclick = () =>{agregarFiltros(12)};
