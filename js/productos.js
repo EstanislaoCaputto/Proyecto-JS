63633038133086431b464f5472dd899e30bcb0bf
Estanislao Caputto <caputto.estanislao@hotmail.com>
2021-06-16 04:18:13 -0300

    creo parte del html desde el js

--- js/productos.js
+++ js/productos.js
@@ -1,125 +1,90 @@
+//Creo los objetos en js
 const tabacos = [];
 const cedas = [];
 const filtros = [];
 
 class item {
-    constructor (nombre, id, precio) {
-        this.nombre = nombre.toUpperCase();
+    constructor (nombre, id, precio, src) {
+        this.nombre = nombre;
         this.id = parseInt(id);
         this.precio = parseInt(precio);
+        this.imagen = src
         
     }
     sumarIva() {
         this.precio = this.precio * 1.21;
     }
+
 }
 
-tabacos.push(new item("Cuatro leguas", 1, 180));
-tabacos.push(new item("Arlequin", 2, 230));
-tabacos.push(new item("Las Hojas", 3, 200));
-tabacos.push(new item("Van Hânsen", 4, 280));
+tabacos.push(new item("Cuatro leguas", 1, 180, src="img/4-leguas.jpg"));
+tabacos.push(new item("Arlequin", 2, 230, src="img/arlequin.jpg"));
+tabacos.push(new item("Las Hojas", 3, 200, src="img/Las-hoja.jpg"));
+tabacos.push(new item("Van Hânsen", 4, 280, src="img/van-hasen.jpg"));
 
-cedas.push(new item("OCB premium", 5, 100));
-cedas.push(new item("OCB cañamo", 6, 90));
-cedas.push(new item("Raw", 7, 70));
-cedas.push(new item("Stamp", 8, 80));
+cedas.push(new item("OCB premium", 5, 100, src="img/cedas-ocb.jpg"));
+cedas.push(new item("OCB cañamo", 6, 90, src="img/cedas-ocb-cañamo.jpg"));
+cedas.push(new item("Raw", 7, 70, src="img/sedas-raw.jpg"));
+cedas.push(new item("Stamp", 8, 80, src="img/sedas-stamps-negras.jpg"));
 
-filtros.push(new item("OCB regular", 9, 120));
-filtros.push(new item("OCB slim", 10, 110));
-filtros.push(new item("Stamp gummed", 11, 130));
-filtros.push(new item("Stamp slim", 12, 110));
+filtros.push(new item("OCB regular", 9, 120, src="img/filtros-ocb-regulares.jpg"));
+filtros.push(new item("OCB slim", 10, 110, src="img/filtros-ocb-slim.jpg"));
+filtros.push(new item("Stamp gummed", 11, 130, src="img/filtros-stamps-engomados.jpg"));
+filtros.push(new item("Stamp slim", 12, 110, src="img/filtros-stamps-slim.jpg"));
 
 
-let mi_tabla = document.createElement("table");
-mi_tabla.setAttribute("class", "table table-warning");
-let mi_tabla_body = document.createElement("tbody");
+//Creo las listas de objetos en el html
 
-for (const producto of tabacos) {
-    let mi_fila = document.createElement("tr");
-    let mi_celda = document.createElement("td");
-    mi_celda.innerText = producto.id;
-    mi_fila.appendChild(mi_celda);
-    let mi_celda2 = document.createElement("td");
-    mi_celda2.innerText = producto.nombre;
-    mi_fila.appendChild(mi_celda2);
-    let mi_celda3 = document.createElement("td");
-    mi_celda3.innerHTML = `<b>$ ${producto.precio}</b>`;
-    mi_fila.appendChild(mi_celda3);
-    mi_tabla_body.appendChild(mi_fila);
+for(t of tabacos){
+    $("#tabacos").append(`
+    <div class= "container">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="${t.imagen}" class="card-img-top" alt="${t.nombre}">
+                    <div class="card-body">
+                    <h5 class="card-title">${t.nombre}</h5>
+                    <p class="card-text">${t.precio}</p>
+                    <a href="#" class="btn btn-success">Comprar</a>
+                </div>
+            </div>
+        </div>
+    </div>`);
+}
+for(f of filtros){
+    $("#filtros").append(`
+    <div class= "container">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="${f.imagen}" class="card-img-top" alt="${f.nombre}">
+                    <div class="card-body">
+                    <h5 class="card-title">${f.nombre}</h5>
+                    <p class="card-text">${f.precio}</p>
+                    <a href="#" class="btn btn-success">Comprar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
 }
 
-mi_tabla.appendChild(mi_tabla_body);
-document.getElementById("tabacos").appendChild(mi_tabla);
-
-
-let mi_tablaCedas = document.createElement("table");
-mi_tablaCedas.setAttribute("class", "table table-warning");
-let mi_tablaCedas_body = document.createElement("tbody");
-
-for (const producto of cedas) {
-    let mi_fila = document.createElement("tr");
-    let mi_celda = document.createElement("td");
-    mi_celda.innerText = producto.id;
-    mi_fila.appendChild(mi_celda);
-    let mi_celda2 = document.createElement("td");
-    mi_celda2.innerText = producto.nombre;
-    mi_fila.appendChild(mi_celda2);
-    let mi_celda3 = document.createElement("td");
-    mi_celda3.innerHTML = `<b>$ ${producto.precio}</b>`;
-    mi_fila.appendChild(mi_celda3);
-    mi_tablaCedas_body.appendChild(mi_fila);
+for(c of cedas){
+    $("#cedas").append(`
+    <div class= "container">
+        <div class="row align-items-center">
+            <div class="col">
+                <div class="card" style="width: 18rem;">
+                    <img src="${c.imagen}" class="card-img-top" alt="${c.nombre}">
+                    <div class="card-body">
+                    <h5 class="card-title">${c.nombre}</h5>
+                    <p class="card-text">${c.precio}</p>
+                    <a href="#" class="btn btn-success">Comprar</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    `);
 }
 
-mi_tablaCedas.appendChild(mi_tablaCedas_body);
-document.getElementById("cedas").appendChild(mi_tablaCedas);
-
-
-let mi_tablaFiltros = document.createElement("table");
-mi_tablaFiltros.setAttribute("class", "table table-warning");
-let mi_tablaFiltros_body = document.createElement("tbody");
-
-for (const producto of filtros) {
-    let mi_fila = document.createElement("tr");
-    let mi_celda = document.createElement("td");
-    mi_celda.innerText = producto.id;
-    mi_fila.appendChild(mi_celda);
-    let mi_celda2 = document.createElement("td");
-    mi_celda2.innerText = producto.nombre;
-    mi_fila.appendChild(mi_celda2);
-    let mi_celda3 = document.createElement("td");
-    mi_celda3.innerHTML = `<b>$ ${producto.precio}</b>`;
-    mi_fila.appendChild(mi_celda3);
-    mi_tablaFiltros_body.appendChild(mi_fila);
-}
-
-mi_tablaFiltros.appendChild(mi_tablaFiltros_body);
-document.getElementById("filtros").appendChild(mi_tablaFiltros)
-
-
-const listaCarrito = [];
-
-function agregarTabaco(id) {
-  const tabacoFind = tabacos.find(producto => producto.id === id);
-  console.log(tabacoFind)
-  listaCarrito.push(tabacoFind)
-}
-
-function agregarCedas(id) {
-    const cedasFind = cedas.find(producto => producto.id === id);
-    console.log(cedasFind)
-    listaCarrito.push(cedasFind)
-}
-
-function agregarFiltros(id) {
-    const filtrosFind = filtros.find(producto => producto.id === id);
-    console.log(filtrosFind)
-    listaCarrito.push(filtrosFind)
-}
-
-
-
-
-
-
-
-console.log(listaCarrito.length);
+//Agrego eventos para añadir al carro
