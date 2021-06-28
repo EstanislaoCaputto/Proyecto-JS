0d6660795271a30bf7fa5e2235b07aec544edc66
Estanislao Caputto <caputto.estanislao@hotmail.com>
2021-06-28 10:47:48 -0300

    formulario (funcional + -)

--- js/formulario.js
+++ js/formulario.js
@@ -1,24 +1,24 @@
 //Se registra el usuario 
 let formValues = {};
 function getFormData(formId){
-    var form1Inputs = document.forms[formId].getElementsByTagName("input");
+    var form1Inputs = document.forms[formId].getElementsByClassName("nomb");
     for(let i=0; i<form1Inputs.length; i++){
           formValues[form1Inputs[i].name]=form1Inputs[i].value;
     }
-    console.log(formValues);
+    // console.log(formValues);
 }
 
 //Envio la info a la API
 const APIURL = 'https://jsonplaceholder.typicode.com/posts';
-const infoPost = getFormData("form1");
+const infoPost = formValues;
 $("#btn1").click(() => { 
     $.ajax({
         method: "POST",
         url:  APIURL,
         data: infoPost,
         success: function(respuesta){
-            $("#cuerpo").append(`<div>Gracias por tu consulta!!</div>`);
-            console.log(respuesta);
+            $("#cuerpo").append(`<div class="card"><div class="card-body">${respuesta.consulta}</div></div>`);
+            // console.log(respuesta);
         }
     });
 });
